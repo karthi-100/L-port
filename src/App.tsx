a135199d43112a762b75ef211623d5563d699875
karthi-100 <karthivicky42@gmail.com>
2026-04-08 21:07:40 +0530

    fix removed unwanted files

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import './App.css'
 import ERSALegalClaude from './components/ERSALegalClaude'
-import ERSALegal from './components/ERSALegal'
 
 function App() {
 
   return (
     <>
-      {/* <ERSALegal/> */}
       <ERSALegalClaude/>
     </>
   )
