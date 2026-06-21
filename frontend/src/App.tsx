aa9e8bc83f88b0bd1cb6c5cf8528ae18cb24db78
karthi-100 <karthivicky42@gmail.com>
2026-06-21 22:15:55 +0530

    fix: added scroll to top when the page is navigated to any part of application

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,17 +4,21 @@ import ERSALegalClaude from './components/ERSALegalClaude'
 import NRIPropertyServices from './components/NRIPropertyServices'
 import IntellectualPropertyServices from './components/IntellectualPropertyServices'
 import CorporateAdvisoryServices from './components/CorporateAdvisoryServices'
+import ScrollToTop from './components/shared/ScrollToTop'
 
 function App() {
 
   return (
     <>
-      <Routes>
+      <ScrollToTop />
+
+        <Routes>
         <Route path="/" element={<ERSALegalClaude/>} />
         <Route path="/nri-property-services" element={<NRIPropertyServices/>} />
         <Route path="/intellectual-property" element={<IntellectualPropertyServices/>} />
         <Route path="/corporate-advisory" element={<CorporateAdvisoryServices/>} />
       </Routes>
+
     </>
   )
 }
